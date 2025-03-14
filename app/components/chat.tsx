b3cb6a37661c0ee72fc1f04b25d86070a1b8b0b7
kayak4665664 <44232222+kayak4665664@users.noreply.github.com>
2025-03-14 11:46:30 +0800

    Update chat.tsx

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1424,8 +1424,8 @@ function _Chat() {
   };
 
   function scrollToBottom() {
-    setMsgRenderIndex(renderMessages.length - CHAT_PAGE_SIZE);
-    scrollDomToBottom();
+    // setMsgRenderIndex(renderMessages.length - CHAT_PAGE_SIZE);
+    // scrollDomToBottom();
   }
 
   // clear context index = context length + index in messages
@@ -1438,8 +1438,7 @@ function _Chat() {
 
   const clientConfig = useMemo(() => getClientConfig(), []);
 
-  // const autoFocus = !isMobileScreen; // wont auto focus on mobile screen
-  const autoFoucs = false;
+  const autoFocus = !isMobileScreen; // wont auto focus on mobile screen
   const showMaxIcon = !isMobileScreen && !clientConfig?.isApp;
 
   useCommand({
